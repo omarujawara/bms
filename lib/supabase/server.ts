ee816fb08d2023d739e474ea7a5648ed99b01692
Omaru Jawara <47614340+omarujawara@users.noreply.github.com>
2026-05-30 07:31:11 -0400

    Refactor createClient function syntax and cookie handling

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -4,17 +4,15 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { type Database } from '@/lib/supabase/database.types'
 
-export async function createClient() {
+export const createClient = async () => {
   const cookieStore = await cookies()
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
       cookies: {
-        getAll() {
-          return cookieStore.getAll()
-        },
-        setAll(cookiesToSet) {
+        getAll: () => cookieStore.getAll(),
+        setAll: (cookiesToSet) => {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
@@ -23,8 +21,8 @@ export async function createClient() {
             // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing sessions.
           }
-        },
-      },
+        }
+      }
     }
   )
 }
